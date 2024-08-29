b870274d0f1dedbf449a96ebc061e52d8262f6aa
edlgld <muramasa3k@gmail.com>
2024-08-29 21:47:58 +0900

    change IP address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,7 +49,7 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-server "35.72.9.13",
+server "52.198.24.184",
    user: "ec2-user",
    roles: %w{web db app},
    ssh_options: {
